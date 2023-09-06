2ea721ee412e9c0ea11a1ee6570577054c95596e
muhsin7865 <76041014+muhsin7865@users.noreply.github.com>
2023-09-06 06:14:42 -0700

    Add files via upload

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,10 @@ export default function Document({ locale }: { locale: any }) {
     <Html lang="en" dir={getDirection(countryCode)}>
       <Head>
         <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+        <meta
           name="description"
           content="Life Pharmacy is a trusted UAE online pharmacy. Order prescription/OTC medicines online. Explore a wide range of vitamins, medicines, beauty brands, baby care products, and sports supplements. Delivery in 30 minutes, Order Now!"
         />
